a044f6b1d8b10a2a1c5f81e99269d47045ab2a7d
SHUBHAM150498 <mishrashubham1504@gmail.com>
2022-12-12 06:47:53 +0530

    project

--- main.js
+++ main.js
@@ -1,130 +1,110 @@
-let form = document.forms['form1'];
-let options1 = form.time1.options;
-let options2 = form.time2.options;
-let options3 = form.time3.options;
-let options4 = form.time4.options;
+function clock(){
+    let time= new Date();
+    let hour=time.getHours();
+    let mint=time.getMinutes();
+    let sec=time.getSeconds();
 
-let timeArray = [];
-let element1 = document.getElementById('hrs');
-let element2 = document.getElementById("mins");
-let element3 = document.getElementById("secs");
-let element4 = document.getElementById("setHalf");
+
+
+document.getElementById("hours").innerHTML = hour+'\n'+' hour';
+document.getElementById("mins").innerHTML= mint + '\n' + ' mins';
+document.getElementById("secs").innerHTML= sec+ '\n'+' secs';
+
+
+if(hour>12){
+    hour = hour-12;
+    document.getElementById("hours").innerHTML = hour+'\n'+ ' hour';
+    document.getElementById("ap").innerHTML="PM"
+}
+else{
+document.getElementById("ap").innerHTML="AM";}
+}
+setInterval(clock);
+
+function valuediv1(){
+    // for wakeup
+    let values1=document.getElementById("wake-select");
+    let valuesMsg1=values1.options[values1.selectedIndex].text;
+    document.getElementById("valueMsg1").innerHTML="Wake-Up Time: " + valuesMsg1;
+
+}
+function valuediv2(){
+        // for party
+        let values2=document.getElementById("lunch-select");
+        let valuesMsg2=values2.options[values2.selectedIndex].text;
+        document.getElementById("valueMsg2").innerHTML="Lunch Time: " + valuesMsg2;
+}
+function valuediv3(){
+    // for nap
+    let values3=document.getElementById("nap-select");
+    let valuesMsg3=values3.options[values3.selectedIndex].text;
+    document.getElementById("valueMsg3").innerHTML="Nap Time: " + valuesMsg3;
+}
+function valuediv4(){
+    // for night
+    let values2=document.getElementById("night-select");
+    let valuesMsg2=values2.options[values2.selectedIndex].text;
+    document.getElementById("valueMsg4").innerHTML="Night Time: " + valuesMsg2;
+}
+
+function settime(){
+    let one = document.getElementById("wake-select").value;
+    let two= document.getElementById("lunch-select").value;
+    let three= document.getElementById("nap-select").value;
+    let four = document.getElementById("night-select").value;
+    let hourr=new Date().getHours();
     
 
-form.onsubmit = function(e) {
-    e.preventDefault();
+    if(one == hourr){
 
-
-    let index1 = options1.selectedIndex;
-    let optionText1 = options1[index1].innerText;
-    timeArray.push(calcTime(optionText1));
-
-    document.getElementById('update1').innerText = "Wake Up Time : " + optionText1;
-
-
-
-
-    let index2 = options2.selectedIndex;
-    let optionText2 = options2[index2].innerText;
-    timeArray.push(calcTime(optionText2));
-
-    document.getElementById('update2').innerText = "Lunch Time : " + optionText2;
-
-
-
-    let index3 = options3.selectedIndex;
-    let optionText3 = options3[index3].innerText;
-    timeArray.push(calcTime(optionText3));
-
-    document.getElementById('update3').innerText = "Nap Time : " + optionText3;
-
-
-
-    let index4 = options4.selectedIndex;
-    let optionText4 = options4[index4].innerText;
-    timeArray.push(calcTime(optionText4));
-
-    document.getElementById('update4').innerText = "Night Time : " + optionText4;
-
-
-
-    for (let i = 0; i < 4; i++) {
-        for(let j = i+1; j < 4; j++) {
-            if(timeArray[i] > timeArray[j]) {
-                
-                let temp = timeArray[i];
-                timeArray[i] = timeArray[j];
-                timeArray[j] = temp;
-            }
-        }
+         document.getElementById("rel_images").style.backgroundImage= "url(./assets/morning.jpg)";
+         document.getElementById("message_one").innerHTML="GOOD MORNING BUDDY!!!";
+       
     }
-    
-    let str = element1.innerText + document.getElementById('setHalf').innerText;
-
-    if(timeArray.length == 4) {
-        if((parseInt(calcTime(str)) >= timeArray[0]) && (parseInt(calcTime(str)) < timeArray[1])) {
-            console.log("case1 passed");
-            document.getElementById("text1").innerText = "GOOD MORNING BUDDY!!!";
-            document.getElementById('text2').innerText = "I hope your morning is as bright as your smile"
-            document.getElementsByClassName("picture")[0].style.backgroundImage = "url(./assets/morning.jpg)"
-    
-        } else if((parseInt(calcTime(str)) >= timeArray[1]) && (parseInt(calcTime(str)) < timeArray[2])) {
-            console.log("case2 passed");
-            document.getElementById("text1").innerText = "LUNCH TIME BUDDY!!!";
-            document.getElementById('text2').innerText = "Lunch is a meal eaten around the middle of the day."
-            document.getElementsByClassName("picture")[0].style.backgroundImage = "url(./assets/lunch.jpg)"
-    
-        } else if((parseInt(calcTime(str)) >= timeArray[2]) && (parseInt(calcTime(str)) < timeArray[3])) {
-            console.log("case3 passed");
-            document.getElementById("text1").innerText = "GOOD EVENING BUDDY!!!";
-            document.getElementById('text2').innerText = "Good evening is a little bit more difficult to say exactly what time it starts and ends, You can read more about “good evening” here.";
-            document.getElementsByClassName("picture")[0].style.backgroundImage =  "url(./assets/lunch.jpg)";
-    
-        } else if((parseInt(calcTime(str)) >= timeArray[3]) || (parseInt(calcTime(str)) < timeArray[0])) {
-            console.log("case4 passed");
-            document.getElementById("text1").innerText = "GOOD NIGHT BUDDY!!!";
-            document.getElementById('text2').innerText = "Good Night Sweet Dreams Take Care.";
-            document.getElementsByClassName("picture")[0].style.backgroundImage = "url(./assets/night.jpg)";
-    
-        } 
-    } 
-
-}
-
-
-
-const calcTime = (t) => {
-    let initial = t.split(" ")[0];
-    if(initial === "12PM") {
-        return 12;
-    } else if (initial === "12AM") {
-        return 0;
-    } else if(initial.includes("PM")) {
-        return parseInt(initial.split("PM")) + 12;
-    } else {
-        return parseInt(initial.split("AM"));
+    else if(two == hourr){
+        document.getElementById("rel_images").style.backgroundImage= "url(./assets/lunch.jpg)";
+        document.getElementById("message_one").innerHTML="LUNCH TIME BUDDY!!!";
     }
+    else if(three == hourr){
+        document.getElementById("rel_images").style.backgroundImage= "url(./assets/evening.jpg)";
+        document.getElementById("message_one").innerHTML="GOOD EVENING BUDDY!!!";
+    }
+    else if(four == hourr){
+        document.getElementById("rel_images").style.backgroundImage= "url(./assets/night.jpg)";
+        document.getElementById("message_one").innerHTML="GOOD NIGHT BUDDY!!!";
+    }
+    else{
+        document.getElementById("rel_images").style.backgroundImage= "url(./assets/welcome.jpg)";
+        document.getElementById("message_one").innerHTML="SET THE ALARAM!!";
+
+    }
+
+    valuediv1();
+    valuediv2();
+    valuediv3();
+    valuediv4();
 }
 
-console.log(timeArray);
-element1.innerText = "2";
+function msg2(){
+    var newtime = new Date();
+    var hrr = newtime.getHours();
+    console.log(hrr);
 
-$(document).ready(function() {
-    $(".alarm").hover(function() {
-        this.innerText = "Set Alarm";
-    }, function() {
-        this.innerText = "Party Time!";
-    });
-});
-
-function clock() {
-    let today = new Date();
-    let hrs = today.getHours();
-    element2.innerText = today.getMinutes();
-    element3.innerText = today.getSeconds();
-
-    element4.innerText = (hrs < 12) ? "AM" : "PM";
-    element1.innerText = (hrs > 12) ? hrs - 12 : hrs;
-
-    var x = setInterval(clock, 1000);
+ if(hrr>=4 && hrr<12){
+    document.getElementById("Message_two").innerHTML="I hope your morning is as bright as your smile";
 }
+else if(hrr>=12 && hrr<16){
+    document.getElementById("Message_two").innerHTML="Lunch is a meal eaten around the middle of the day.";
+}
+else if(hrr>=16 && hrr<20){
+  
+    document.getElementById("Message_two").innerHTML="Good evening is a little bit more difficult to say exactly what time it starts and ends, You can read more about “good evening” here";
+}
+else if(hrr>=20 || hrr<4){
+    document.getElementById("Message_two").innerHTML="Good Night Sweet Dreams Take Care.";
+}
+
+
+}
+
+msg2();
